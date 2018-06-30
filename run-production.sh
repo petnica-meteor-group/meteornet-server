62885360db9c0c96a220f0c96378095d54261900
Vladimir Nikolić <vladimir.nik94@gmail.com>
2018-06-30 23:02:43 +0200

    Update run-production.sh

--- run-production.sh
+++ run-production.sh
@@ -6,7 +6,7 @@ sudo ./manage.py collectstatic --noinput
 
 sudo mkdir -p $NGINX_SERVERS_DIR
 sudo ln -sf $PROJECT_DIR/$MAIN_APP/nginx.conf $NGINX_SERVERS_DIR/$MAIN_APP.conf
-sudo systemctl start nginx
+sudo systemctl restart nginx
 
 sudo uwsgi --ini $MAIN_APP/uwsgi.ini --uid=$USER --gid=$USER
 sudo rm -f $SOCKET_PATH
