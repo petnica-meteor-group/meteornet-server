5d6407f5bf232b7da149a1bbbe622cf7f8b918eb
Vladimir Nikolić <vladimir.nik94@gmail.com>
2018-07-08 18:43:55 +0200

    Update run-production.sh

--- run-production.sh
+++ run-production.sh
@@ -8,5 +8,11 @@ sudo mkdir -p $NGINX_SERVERS_DIR
 sudo ln -sf $PROJECT_DIR/$MAIN_APP/nginx.conf $NGINX_SERVERS_DIR/$MAIN_APP.conf
 sudo systemctl restart nginx
 
-sudo uwsgi --ini $MAIN_APP/uwsgi.ini --uid=$USER --gid=$USER
+if [[ $USER == root ]]; then
+    user=$SUDO_USER
+else
+    user=$USER
+fi
+
+sudo uwsgi --ini $MAIN_APP/uwsgi.ini --uid=$user --gid=$user
 sudo rm -f $SOCKET_PATH
