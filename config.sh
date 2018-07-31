487dbf4cf447424340f2c8cbad383aed4d50d152
schutzekatze <vladimir.nik94@gmail.com>
2018-07-31 20:51:11 +0200

    Add domain name

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 PROJECT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 MAIN_APP=meteor_network_server
-DOMAIN_NAME=mns.rs
+DOMAIN_NAME=meteori.petnica.rs
 
 STATIC_DIR=/srv/http/$MAIN_APP/static
 MEDIA_DIR=/srv/http/$MAIN_APP/media
