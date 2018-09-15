e0ff8072c8a04a18fb7bc1294f63ae65d2b5e8fc
schutzekatze <vladimir.nik94@gmail.com>
2018-09-15 21:33:08 +0200

    Update

--- update-station-code.sh
+++ update-station-code.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd meteor_network_server/stations/station_code && git pull origin master && cd .. \
+cd meteor_network_server/stations/station_code && git pull --recurse-submodules origin master && cd .. \
    && rm -rf station_code.zip && zip -r station_code.zip station_code
